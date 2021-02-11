52549eb9dec1e75ea4c9f1f68e404cffca025471
clevelandtwal <69008094+clevelandtwal@users.noreply.github.com>
2021-02-11 16:56:35 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# FitTracker
-Fitness Tracker
+# FitnessTracker
+
+The Fitness Tracker application collect a user's data and and stores it so that they can track their fitness activities over time.
+
+# Technologies Used
+The project uses NodeJS, ExpressJS, Mongoose and MongoDB. 
+
+Upon pageload, historical fitness data or "workout" datais displayed calling an API to fetch the data. Users may add their most recent workout by completing a form and is stored in MongoDB. 
---
+++ seeders/seed.js
@@ -0,0 +1,137 @@
+let mongoose = require("mongoose");
+let db = require("../models");
+
+mongoose.connect("mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+
+let workoutSeed = [
+  {
+    day: new Date().setDate(new Date().getDate()-10),
+    exercises: [
+      {
+        type: "resistance",
+        name: "Bench press",
+        duration: 20,
+        weight: 100,
+        reps: 10,
+        sets: 4
+      }
+    ]
+  },
+  {
+    day: new Date().setDate(new Date().getDate()-9),
+    exercises: [
+      {
+        type: "Resistance setting",
+        name: "Lat pull",
+        duration: 20,
+        weight: 300,
+        reps: 10,
+        sets: 4
+      }
+    ]
+  },
+  {
+    day: new Date().setDate(new Date().getDate()-8),
+    exercises: [
+      {
+        type: "Resistance setting",
+        name: "Military press",
+        duration: 25,
+        weight: 185,
+        reps: 8,
+        sets: 4
+      }
+    ]
+  },
+  {
+    day: new Date().setDate(new Date().getDate()-7),
+    exercises: [
+      {
+        type: "Cardio",
+        name: "Jogging",
+        duration: 25,
+        distance: 4
+      }
+    ]
+  },
+  {
+    day: new Date().setDate(new Date().getDate()-6),
+    exercises: [
+      {
+        type: "Resistance setting",
+        name: "Leg press",
+        duration: 20,
+        weight: 285,
+        reps: 10,
+        sets: 4
+      }
+    ]
+  },
+  {
+    day: new Date().setDate(new Date().getDate()-5),
+    exercises: [
+      {
+        type: "Resistance setting",
+        name: "Dumbell press",
+        duration: 20,
+        weight: 300,
+        reps: 10,
+        sets: 4
+      }
+    ]
+  },
+  {
+    day: new Date(new Date().setDate(new Date().getDate() - 4)),
+    exercises: [
+      {
+        type: "Resistance setting",
+        name: "Quad Press",
+        duration: 30,
+        weight: 300,
+        reps: 10,
+        sets: 4
+      }
+    ]
+  },
+  {
+    day: new Date(new Date().setDate(new Date().getDate() - 3)),
+    exercises: [
+      {
+        type: "Resistance setting",
+        name: "Bench Press",
+        duration: 20,
+        weight: 300,
+        reps: 10,
+        sets: 4
+      }
+    ]
+  },
+  {
+    day: new Date(new Date().setDate(new Date().getDate() - 2)),
+    exercises: [
+      {
+        type: "Resistance setting",
+        name: "Military press",
+        duration: 20,
+        weight: 300,
+        reps: 10,
+        sets: 4
+      }
+    ]
+  }
+];
+
+db.Workout.deleteMany({})
+  .then(() => db.Workout.collection.insertMany(workoutSeed))
+  .then(data => {
+    console.log(data.result.n + " added");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
